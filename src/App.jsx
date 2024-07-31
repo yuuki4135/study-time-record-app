44e102d1cb1469f9cf8517563256e2e85dff9690
sugimoto.yuki <yuuki4135@gmail.com>
2024-07-31 22:10:01 +0900

    mvp1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 
 function App() {
+  const records = [
+    { title: "勉強の記録1", time: 1},
+    { title: "勉強の記録2", time: 3},
+    { title: "勉強の記録3", time: 5}
+  ]
 
   return (
     <>
       <h1>学習記録一覧</h1>
+        {records.map((record, index) => (
+          <p key={index}>{`${record.title} ${record.time}時間`}</p>
+        ))}
     </>
   )
 }
